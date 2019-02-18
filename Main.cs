1a84e61455ffcd2904d35c31742a411fa7adebb0
ThomasSoerensen1996 <thomas10001@live.dk>
2019-02-18 12:39:07 +0100

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wox.Plugin;
+
+namespace UnitConverter
+{
+    public class Main : IPlugin
+    {
+        public List<Result> Query(Query query)
+        { 
+            List<Result> results = new List<Result>();
+
+           
+            UnitConverter unitConverter = new UnitConverter();
+
+            string[] q = query.RawQuery.Split(' ');
+
+            string distance = q[1];
+            string convertFrom = q[2];
+            string convertTo = q[3];
+
+            unitConverter.unitBase = Convert.ToDouble(distance);
+            unitConverter.unitConvert(convertFrom);
+            unitConverter.unitFinal(convertTo);
+
+            Result r = new Result();
+            r.Title = "Unit Converter";
+            r.SubTitle = ""+unitConverter.unitConverted;
+            results.Add(r);
+
+            return results;
+        }
+
+        public void Init(PluginInitContext context)
+        {
+            
+        }
+    }
+}
---
+++ UnitConverter.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConverter
+{
+    class UnitConverter
+    {
+        private double unitStandard;
+        public double unitBase;
+        public double unitConverted;
+
+        public void unitConvert(String Value1)
+        {
+            switch (Value1)
+            {
+                case "millimeter":
+                    {
+                        unitStandard = unitBase / 1000;
+                        break;
+                    }
+
+
+                case "centimeter":
+                    {
+                        unitStandard = unitBase / 100;
+                        break;
+                    }
+
+                case "meter":
+                    {
+                        unitStandard = unitBase;
+                        break;
+                    }
+
+                case "kilometer":
+                    {
+                        unitStandard = unitBase * 22;
+                        break;
+                    }
+
+                case "inch":
+                    {
+                        unitStandard = unitBase / 39.37007874;
+                        break;
+                    }
+
+                case "feet":
+                    {
+                        unitStandard = unitBase / 3.2808399;
+                        break;
+                    }
+
+                case "yard":
+                    {
+                        unitStandard = unitBase / 1.0936133;
+                        break;
+                    }
+
+                case "mile":
+                    {
+                        unitStandard = unitBase * 1609.344;
+                        break;
+                    }
+            }
+        }
+
+        public void unitFinal(String Value2)
+        {
+            switch (Value2)
+            {
+                case "millimeter":
+                    {
+                        unitConverted = unitStandard * 1000;
+                        break;
+                    }
+
+
+                case "centimeter":
+                    {
+                        unitConverted = unitStandard * 100;
+                        break;
+                    }
+
+                case "meter":
+                    {
+                        unitConverted = unitStandard;
+                        break;
+                    }
+
+                case "kilometer":
+                    {
+                        unitConverted = unitStandard / 2;
+                        break;
+                    }
+
+                case "inch":
+                    {
+                        unitConverted = unitStandard * 39.37007874;
+                        break;
+                    }
+
+                case "feet":
+                    {
+                        unitConverted = unitStandard * 3.2808399;
+                        break;
+                    }
+
+                case "yard":
+                    {
+                        unitConverted = unitStandard * 1.0936133;
+                        break;
+                    }
+
+                case "mile":
+                    {
+                        unitConverted = unitStandard / 1609.344;
+                        break;
+                    }
+            }
+        }
+    }
+}
